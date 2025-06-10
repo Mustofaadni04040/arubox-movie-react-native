f9c608ff2de7e94f32f4941ee3a9fe54c2cff046
mustofa adny <adniadni04@gmail.com>
2025-06-10 17:44:39 +0700

    ui: search bar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { Image, ScrollView, View } from "react-native";
@@ -7,11 +8,19 @@ export default function Index() {
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0" />
 
-      <ScrollView className="flex-1 px-5">
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
         <Image
           source={icons.logo}
           className="w-fit h-auto mt-20 mb-5 mx-auto"
         />
+
+        <View className="flex-1 mt-5">
+          <SearchBar />
+        </View>
       </ScrollView>
     </View>
   );
---
+++ components/SearchBar.tsx
@@ -0,0 +1,24 @@
+import { icons } from "@/constants/icons";
+import React from "react";
+import { Image, TextInput, View } from "react-native";
+
+export default function SearchBar() {
+  return (
+    <View className="flex flex-row gap-3 items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        className="size-5"
+        resizeMode="contain"
+        source={icons.search}
+        tintColor="#FFBCCF"
+      />
+      <TextInput
+        onPress={() => {}}
+        placeholder="Cari judul film disini..."
+        value=""
+        onChangeText={() => {}}
+        placeholderTextColor={"#fff"}
+        className="flex-1 ml-2 text-white placeholder:opacity-50"
+      />
+    </View>
+  );
+}
