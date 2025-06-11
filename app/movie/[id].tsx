c27628e2892715bada172f72de679e8ca939af3c
mustofa adny <adniadni04@gmail.com>
2025-06-11 16:15:39 +0700

    feat: func fetch movie details

--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { ScrollView, View } from "react-native";
 
 export default function MovieDetails() {
   return (
-    <View>
-      <Text>id</Text>
+    <View className="bg-primary flex-1">
+      <ScrollView
+        contentContainerStyle={{
+          paddingBottom: 80,
+        }}
+      ></ScrollView>
     </View>
   );
 }
